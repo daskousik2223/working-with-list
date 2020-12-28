6b5cea0746d62c14c7fcce9433968d31980c8c5c
daskousik2223 <das.kousik2223@gmail.com>
2020-12-28 14:30:27 -0500

    commit changes

---
+++ src/Person/Person.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const person = (props) => {
+    return (
+        <div>
+            <p onClick={props.click}>I am {props.name} and my color is {props.color} - Price : {props.price}</p>
+            <p>New Price : <input type="text" onChange={props.changed}/></p>
+        </div>
+    );
+}
+
+export default person;
